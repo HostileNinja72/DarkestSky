74df16a76be1efdd43e01a16c0ce8859e82c491a
Soufiane47 <soufianeastronomie@gmail.com>
2024-08-26 02:20:38 +0100

    changes are coming

--- gui/gui.py
+++ gui/gui.py
@@ -1,53 +1,47 @@
 import sys
 import os
+import folium
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
-import geemap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Google Earth Engine with PyQt5")
+        self.setWindowTitle("GUI testing/learning")
         self.setGeometry(100, 100, 800, 600)
         self.initUI()
 
     def initUI(self):
-        # Create a central widget
+    
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Create a layout for the central widget
+        
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
 
-        # Create a QWebEngineView
+       
         self.web_view = QWebEngineView()
         layout.addWidget(self.web_view)
 
-        # Create a map using geemap
-        self.map_widget = geemap.Map()
-        self.map_widget.add_basemap('HYBRID')
+        
+        folium_map = folium.Map(
+            location=[33.53210, -5.10950],
+            zoom_start=13,
+            tiles="CartoDB dark_matter", # dark theme
+            min_zoom=3,
+            max_bounds = True # Set the minimum zoom level
+        )
+         
+        """cartodb positron"""
+        folium.CircleMarker(location=[33.53210, -5.10950 ], fill=True).add_to(folium_map)
 
-        # Example coordinates (ensure these are valid)
-        lat, lon = 31.16580958786196, -7.9375  # Replace with your actual coordinates
+        
+        html_file = os.path.abspath('folium_map.html')
+        folium_map.save(html_file)
 
-        # Debugging: Print the coordinates
-        print(f"Latitude: {lat}, Longitude: {lon}")
-
-        # Validate coordinates
-        if not (isinstance(lat, (int, float)) and isinstance(lon, (int, float))):
-            print("Invalid coordinates")
-            return
-
-        # Center the map on the coordinates
-        self.map_widget.setCenter(lat, lon, 10)
-
-        # Save the map as an HTML file
-        html_file = os.path.abspath('map.html')
-        self.map_widget.to_html(html_file)
-
-        # Load the HTML file in the QWebEngineView
+       
         self.web_view.setUrl(QUrl.fromLocalFile(html_file))
 
 if __name__ == "__main__":
